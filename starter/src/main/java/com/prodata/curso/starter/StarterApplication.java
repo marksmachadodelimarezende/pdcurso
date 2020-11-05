eac94865662ca58cd2281b98c30a45836b318522
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2020-11-05 00:34:50 -0300

    Implementado Cors para determinar as origens permitidas de requisiçoes, e quais verbos podem ser utilizados. Foi liberado get, post, put e delete.
    Adicionado instrucao de instalacao no build.gradle.

--- starter/src/main/java/com/prodata/curso/starter/StarterApplication.java
+++ starter/src/main/java/com/prodata/curso/starter/StarterApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.sql.SQLException;
 
@@ -13,6 +15,20 @@ public class StarterApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StarterApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		//Adicionar dependencia do HttpClient no build.gradle - ref: https://spring.io/guides/gs/rest-service-cors/
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**") //Aqui habilita qualquer chamada dentro das origens configuradas abaixo.
+						.allowedMethods("GET","POST","PUT","DELETE")
+						.allowedHeaders("*")
+						.allowedOrigins("*");
+			}
+		};
 	}
 
 	@Bean
