ed7cd1cea04ccd01353058f62fe89c02f46efc0a
marksmachadodelimarezende <marksmlr.dev@gmail.com>
2020-11-07 01:13:19 -0300

    Implementação de dependencias do JasperReports no gradle. Pasta de relatorios jasper movida para raiz do projeto. Criado metodo no controller para imprimir relatorio.

--- starter/src/main/java/com/prodata/curso/starter/controller/ContatoController.java
+++ starter/src/main/java/com/prodata/curso/starter/controller/ContatoController.java
@@ -1,10 +1,13 @@
 package com.prodata.curso.starter.controller;
 
+import com.prodata.curso.starter.core.RunReportJasper;
 import com.prodata.curso.starter.entity.Contato;
 import com.prodata.curso.starter.service.ServicoContatoCRUD;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 import java.util.Map;
 
@@ -40,5 +43,10 @@ public class ContatoController {
     @DeleteMapping("deleteId")
     public ResponseEntity<Map<String, Object>> deleteId(@RequestParam long id) throws Exception {
         return ResponseEntity.ok(new ServicoContatoCRUD().deleteId(id));
+    }
+
+    @PostMapping("print")
+    public void print(HttpServletRequest request, HttpServletResponse response, @RequestBody Map<String, Object> params){
+        new RunReportJasper().printReportPdf(request, response, params);
     }
 }
--- starter/src/main/java/com/prodata/curso/starter/core/RunReportJasper.java
+++ starter/src/main/java/com/prodata/curso/starter/core/RunReportJasper.java
@@ -1,4 +1,56 @@
 package com.prodata.curso.starter.core;
 
-public class RunReportJasper {
+import core.db.Conexao;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+public class RunReportJasper extends Conexao {
+
+    public void printReportPdf(HttpServletRequest request,HttpServletResponse response, Map<String, Object> params) {
+        String pathReportJasper = "/reports/gerencial/listaContato.jasper";
+        String[] arrayReportJasper = pathReportJasper.split("/");
+        String nameReportJasper = arrayReportJasper[arrayReportJasper.length - 1];
+        pathReportJasper = pathReportJasper.replace(nameReportJasper, "");
+        String subReportDir = new File("").getAbsolutePath()+pathReportJasper;
+        params.put("SUBREPORT_DIR",  subReportDir);
+
+        String nameReportPdf = nameReportJasper.replace(".jasper", ".pdf");
+
+        params.put("clausulaWhere", "Where true");
+        params.put("nome_empresa", "Curso AngularJs + Spring Boot + PostgreSQL");
+        params.put("titulo", "Listagem de contatos");
+
+        try (Connection con = getCon()) {
+            Path path = Paths.get(subReportDir+nameReportJasper);
+            InputStream jasperStream = new FileInputStream(path.toString());
+            JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, con);
+            byte[] reportByte = JasperRunManager.runReportToPdf(path.toString(), params, con);
+
+            //Adicionando relatorio para download
+            response.setContentType("application/x-pdf");
+            response.setHeader("Content-disposition", "inline; filename=" + nameReportPdf);
+
+            //Adicionar relatorio no Servlet
+            OutputStream outputStream = response.getOutputStream();
+            //JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+            outputStream.write(reportByte);
+            outputStream.flush();
+            outputStream.close();
+
+            jasperStream.close();
+
+        } catch (JRException | SQLException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
